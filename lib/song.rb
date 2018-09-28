bc217f491280a12ccdb4bd6bcf273107b7f48e6c
Mac Dowell Prosper <macdowell@noukod.com>
2018-09-28 19:12:39 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -15,11 +15,13 @@ class Song
     song.save
     song
   end
+  
   def self.new_by_name(name)
     song = Song.new
     song.name = name
     song
   end
+  
   def self.create_by_name(name)
     song = self.create
     song.name = name
@@ -38,6 +40,7 @@ class Song
   def self.alphabetical
     self.all.sort_by {|song| song.name}
   end
+  
   def self.new_from_filename(filename)
     parts = filename.split(" - ")
     artist_name = parts[0]
@@ -48,15 +51,19 @@ class Song
     song.artist_name = artist_name
     song
   end
+  
   def self.create_from_filename(filename)
     parts = filename.split(" - ")
     artist_name = parts[0]
     song_name = parts[1].gsub(".mp3", "")
 
-    song = self.create
-    song.name = song_name
+    song = self.create_by_name(song_name)
     song.artist_name = artist_name
     song
+  end
+  
+  def self.destroy_all
+    self.all.clear
   end
 
 end
